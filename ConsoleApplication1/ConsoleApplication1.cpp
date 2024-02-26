ccac99d678cf54378f6d7f491c3b9dada0615916
SyandyArda <syandi.2901@gmail.com>
2024-02-26 12:04:11 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin
+        // numeric nJejari, nLuas
+        // display ' Masukan jejari= ;
+        // accept njejari
+        // compute nLuas = 3.14 * r * r
+        // display 'Luasnya= ' + nLuas
+    //end
+
+    double nJejari, N=nLuas
+}
+
